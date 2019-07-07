18953bc914839e0140cc63a215cdfd529aae845c
Jonathan Andrews <48098383+Jonathan-Andrews@users.noreply.github.com>
2019-07-07 14:26:32 +0100

    Update initializers.py

--- dlmb/initializers.py
+++ dlmb/initializers.py
@@ -14,6 +14,7 @@ def zeros(matrix_shape):
 
 	return np.zeros(matrix_shape)
 
+# ------------------------------------------------------------------------------------------------------------------------
 def ones(matrix_shape):
 	"""
 	Creates a matrix of shape matrix_shape filled with values of 1.
@@ -28,6 +29,7 @@ def ones(matrix_shape):
 
 	return np.ones(matrix_shape)
 
+# ------------------------------------------------------------------------------------------------------------------------
 def random(matrix_shape):
 	"""
 	Creates a matrix of shape matrix_shape filled with random values between 0 and 1.
@@ -42,6 +44,7 @@ def random(matrix_shape):
 
 	return np.random.random(matrix_shape)
 
+# ------------------------------------------------------------------------------------------------------------------------
 def uniform(matrix_shape):
 	"""
 	Creates a uniform matrix with boundaries of [-1/sqrt(n), 1/sqrt(n)].
@@ -57,6 +60,7 @@ def uniform(matrix_shape):
 	boundaries = 1/np.sqrt(matrix_shape[1])
 	return np.random.uniform(-boundaries, boundaries, matrix_shape)
 
+# ------------------------------------------------------------------------------------------------------------------------
 def xavier(matrix_shape):
 	"""
 	Creates a gaussian distribution matrix with a mean of 0 and variance of sqrt(2/(n+m)) where n and m are the sizes of the matrix.
@@ -71,6 +75,7 @@ def xavier(matrix_shape):
 
 	return np.random.normal(0, np.sqrt(2/(matrix_shape[0]+matrix_shape[1])), matrix_shape)
 
+# ------------------------------------------------------------------------------------------------------------------------
 def xavier_uniform(matrix_shape):
 	"""
 	Creates a uniform matrix based of the xavier initializer with boundaries of [-sqrt(6)/sqrt(n+m), sqrt(6)/sqrt(n+m)] where n and m are the sizes of the matrix.
@@ -86,6 +91,7 @@ def xavier_uniform(matrix_shape):
 	boundaries = np.sqrt(6)/np.sqrt(matrix_shape[0]+matrix_shape[1])
 	return np.random.uniform(-boundaries, boundaries, matrix_shape)
 
+# ------------------------------------------------------------------------------------------------------------------------
 def sigmoid_uniform(matrix_shape):
 	"""
 	Creates a uniform matrix based of the xavier_uniform initializer with boundaries of [-(4*sqrt(6))/sqrt(n+m), 4*sqrt(6)/sqrt(n+m)] where n and m are the sizes of the matrix.
@@ -101,6 +107,7 @@ def sigmoid_uniform(matrix_shape):
 	boundaries = 4*np.sqrt(6)/np.sqrt(matrix_shape[0]+matrix_shape[1])
 	return np.random.uniform(-boundaries, boundaries, matrix_shape)
 
+# ------------------------------------------------------------------------------------------------------------------------
 def relu(matrix_shape):
 	"""
 	Creates a gaussian distribution matrix with a mean of 0 and variance of sqrt(2/m) where m is the number of rows
@@ -115,6 +122,7 @@ def relu(matrix_shape):
 
 	return np.random.normal(0, np.sqrt(2/matrix_shape[0]), matrix_shape)
 
+# ------------------------------------------------------------------------------------------------------------------------
 def relu_uniform(matrix_shape):
 	"""
 	Creates a uniform matrix based of the relu initializer with boundaries of [-sqrt(6/m), sqrt(6/m)] where m is the number of rows
