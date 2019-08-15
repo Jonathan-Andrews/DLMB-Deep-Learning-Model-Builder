a81ab070763d3973b7b4a9f6c3890e700c3485e4
Jonathan Andrews <48098383+Jonathan-Andrews@users.noreply.github.com>
2019-08-15 12:30:14 +0100

    Update batchnorm.py

--- dlmb/layers/batchnorm.py
+++ dlmb/layers/batchnorm.py
@@ -5,38 +5,31 @@ import initializers as I
 import regularizers as R 
 
 class Batchnorm(Base_layer):
-	def __init__(self, beta_initializer="random", gamma_initializer="random", beta_regularizer="L0", gamma_regularizer="L0", epsilon=1e-8):
+	def __init__(self, gamma_initializer="random", beta_initializer="random", gamma_regularizer="L0", beta_regularizer="L0", epsilon=1e-8,
+		         trainable=True, *args, **kwargs):
 		"""
-		The Batchnorm class takes some data and scales and shifts it for better flow between the other layers.
-		The Batchnorm class solves problems like exploding or vanishing gradients.
+		The Batchnorm class takes some data and scales and shifts it for better flow between the other layers,
+		which solves problems like exploding or vanishing gradients.
 		
 		Arguments:
-			beta_initializer - type = Str: An initializer function to set the value for the beta var.
-			gamma_initializer - type = Str: An initializer function to set the value for the gamma var.
-			beta_regularizer - type = Str: A regularizer function that regularizes the beta var.
-			gamma_regularizer - type = Str: A regularizer function that regularizes the gamma var.
-			epsilon - type = Float: A very small number used in the calculations for the map_data() function.
+			gamma_initializer - str/instance of a function: An initializer function to set the value for the gamma var.
+			beta_initializer - str/instance of a function: An initializer function to set the value for the beta var.
+			gamma_regularizer -  str/instance of a regularizer class: A regularizer function that regularizes the gamma var.
+			beta_regularizer -  str/instance of a regularizer class: A regularizer function that regularizes the beta var.
+			epsilon - float/int: A very small number used in the calculations for the map_data() function.
 
 		"""
 
-		self.layer_type = "Batchnorm"
+		self.name = "Batchnorm"
 		self.built = False
-		self.beta_initializer = beta_initializer
-		self.gamma_initializer = gamma_initializer
-		self.beta_regularizer = beta_regularizer
-		self.gamma_regularizer = gamma_regularizer
-		self.epsilon = epsilon
+		self.trainable = trainable
 
-		# Stores the optimizations for the trainable vars.
-		self.beta_optimizations = [0, 0]
-		self.gamma_optimizations = [0, 0]
+		self.initializers = {"gamma":gamma_initializer, "beta":beta_initializer}
+		self.regularizers = {"gamma":R.get(gamma_regularizer), "beta":R.get(beta_regularizer)}
 
-		# Stores the beta and the gamma
-		self.beta = []
-		self.gamma = []
-
-		# Stores the mapped data in a dict.
-		self.cached_vars = {}
+		self.vars = {"gamma":0, "beta":0, "epsilon":epsilon}
+		self.optimizations = {"gamma":[0,0], "beta":[0,0]}
+		self.cached_data = {}
 
  	# ------------------------------------------------------------------------------------------------------------------------
 	def set_vars(self):
@@ -45,69 +38,60 @@ class Batchnorm(Base_layer):
 
 		"""
 
-		if self.built != True:
-			# Sets the value for the beta var based on self.beta_initializer.
-			if self.beta_initializer.lower() in ("zeros", "zero"):
-				self.beta = I.zeros((1,1))
-			elif self.beta_initializer.lower() in ("ones", "one"):
-				self.beta = I.ones((1,1))
-			elif self.beta_initializer.lower() in ("random"):
-				self.beta = I.random((1,1))
-			elif self.beta_initializer.lower() in ("uniform"):
-				self.beta = I.uniform((1,1))
-			elif self.beta_initializer.lower() in ("xavier"):
-				self.beta = I.xavier((1,1))
-			elif self.beta_initializer.lower() in ("xavier_uniform"):
-				self.beta = I.xavier_uniform((1,1))
-			elif self.beta_initializer.lower() in ("sigmoid_uniform"):
-				self.beta = I.sigmoid_uniform((1,1))
-			elif self.beta_initializer.lower() in ("relu"):
-				self.beta = I.relu((1,1))
-			elif self.beta_initializer.lower() in ("relu_uniform"):
-				self.beta = I.relu_uniform((1,1))
-			else:
-				raise ValueError(f"{self.beta_initializer} is not an available beta initializer")
+		if not self.built:
+			# Set self.vars["gamma"] based on self.initializers["gamma"].
+			self.vars["gamma"] = I.get(self.initializers["gamma"])((1,1))
 
-			# Sets the value for the gamma var based on self.gamma_initializer.
-			if self.gamma_initializer.lower() in ("zeros", "zero"):
-				self.gamma = I.zeros((1,1))
-			elif self.gamma_initializer.lower() in ("ones", "one"):
-				self.gamma = I.ones((1,1))
-			elif self.gamma_initializer.lower() in ("random"):
-				self.gamma = I.random((1,1))
-			elif self.gamma_initializer.lower() in ("uniform"):
-				self.gamma = I.uniform((1,1))
-			elif self.gamma_initializer.lower() in ("xavier"):
-				self.gamma = I.xavier((1,1))
-			elif self.gamma_initializer.lower() in ("xavier_uniform"):
-				self.gamma = I.xavier_uniform((1,1))
-			elif self.gamma_initializer.lower() in ("sigmoid_uniform"):
-				self.gamma = I.sigmoid_uniform((1,1))
-			elif self.gamma_initializer.lower() in ("relu"):
-				self.gamma = I.relu((1,1))
-			elif self.gamma_initializer.lower() in ("relu_uniform"):
-				self.gamma = I.relu_uniform((1,1))
-			else:
-				raise ValueError(f"{self.gamma_initializer} is not an available gamma initializer")
-
-		# Sets the regularizers if the user sent through a string as the argument.
-		if type(self.beta_regularizer) == str:
-			if self.beta_regularizer.lower() in ("l0"):
-				self.beta_regularizer = R.L0()
-			elif self.beta_regularizer.lower() in ("l1"):
-				self.beta_regularizer = R.L1()
-			elif self.beta_regularizer.lower() in ("l2"):
-				self.beta_regularizer = R.L2()
-
-		if type(self.gamma_regularizer) == str:
-			if self.gamma_regularizer.lower() in ("l0"):
-				self.gamma_regularizer = R.L0()
-			elif self.gamma_regularizer.lower() in ("l1"):
-				self.gamma_regularizer = R.L1()
-			elif self.gamma_regularizer.lower() in ("l2"):
-				self.gamma_regularizer = R.L2()
+			# Set self.vars["beta"] based on self.initializers["beta"].
+			self.vars["beta"] = I.get(self.initializers["beta"])((1,1))
 
 		self.built = True
+
+	# ------------------------------------------------------------------------------------------------------------------------
+	def get_summary(self, with_vars, *args):
+		"""
+		get_summary() returns a summary of the layers features.
+
+		Arguments:
+			with_vars - bool: If True, get_summary() includes the layer's variables' values in the summary.
+
+		Returns:
+			summary - dict: A dictonary of the layers features.
+
+		"""
+
+		summary = {
+					"name":self.name,
+					"built":self.built,
+					"trainable":self.trainable,
+					"initializers":self.initializers,
+					"regularizers":{key:self.regularizers[key].name for key in self.regularizers},
+					"num_trainable_vars":2
+				  }
+
+		if with_vars:
+			summary["vars"] = {key:np.asarray(self.vars[key]).tolist() for key in self.vars}
+			summary["optimizations"] = {key:np.asarray(self.optimizations[key]).tolist() for key in self.optimizations}
+
+		return summary
+
+	# ------------------------------------------------------------------------------------------------------------------------
+	def load(self, layer_data, *args, **kwargs):
+		"""
+		Takes the layer_data from the model this layer belongs to and sets all vars equal to each key in layer_data.
+
+		Arguments:
+			layer_data - dict: A dictonary of saved vars from when this layer was first built and then saved.
+
+		"""
+
+		self.name = layer_data["name"]
+		self.built = layer_data["built"]
+		self.trainable = layer_data["trainable"]
+		self.initializers = layer_data["initializers"]
+		self.regularizers = {key:R.get(layer_data["regularizers"][key]) for key in layer_data["regularizers"]}
+		self.vars = {key:np.asarray(layer_data["vars"][key]) for key in layer_data["vars"]}
+		self.optimizations = {key:np.asarray(layer_data["optimizations"][key]) for key in layer_data["optimizations"]}
 
  	# ------------------------------------------------------------------------------------------------------------------------
 	def map_data(self, data):
@@ -115,20 +99,20 @@ class Batchnorm(Base_layer):
 		Maps the data to an output by shifting and scaling the incoming input.
 
 		Arguments:
-			data - type = Numpy array: A matrix or vector (depending on batch size) containing real numbers passed from the previous layer.
+			data - Numpy array: A numpy array containing real numbers passed from the previous layer.
 
 		Returns:
-			output - type = Numpy array: A matrix or vector (depending on batch size) of outputs from this layer.
+			output - Numpy array: A numpy array of outputs from this layer.
 
 		"""
 
-		self.cached_vars["data"] = data
+		self.cached_data["data"] = data
 
-		self.cached_vars["mu"] = np.mean(data, axis=1, keepdims=True)
-		self.cached_vars["var"] = np.var(data, axis=1, keepdims=True)
-		self.cached_vars["x_norm"] = (data-self.cached_vars["mu"]) / np.sqrt(self.cached_vars["var"]+self.epsilon)
+		self.cached_data["mu"] = np.mean(data, axis=1, keepdims=True)
+		self.cached_data["var"] = np.var(data, axis=1, keepdims=True)
+		self.cached_data["x_norm"] = (data-self.cached_data["mu"]) / np.sqrt(self.cached_data["var"]+self.vars["epsilon"])
 
-		return self.gamma*self.cached_vars["x_norm"] + self.beta
+		return self.vars["gamma"]*self.cached_data["x_norm"] + self.vars["beta"]
 
  	# ------------------------------------------------------------------------------------------------------------------------
 	def calculate_gradients(self, error):
@@ -136,29 +120,29 @@ class Batchnorm(Base_layer):
 		Calculates the partial derivative of the error W.R.T each trainable var in the layer. 
 
 		Arguments:
-			error - type = Numpy array: A matrix or vector (depending on batch size) containing the error for the previous layer.
+			error - Numpy array: A numpy array containing the error for the previous layer.
 
 		Returns:
-			output - type = Numpy array: A matrix or vector (depending on batch size) containing the error for this layer.
+			output - Numpy array: A numpy array containing the error for this layer.
 
 		"""
 
 		# Calculate the beta gradients.
-		self.beta_optimizations[0] = np.mean(error) # + self.beta_regularizer.function(self.beta) 
+		self.optimizations["beta"][0] = np.mean(error) + self.regularizers["beta"].map_data(self.vars["beta"]) 
 
 		# Calculate the gamma gradients.
-		self.gamma_optimizations[0] = np.mean(error*self.cached_vars["x_norm"]) # + self.gamma_regularizer.function(self.gamma) 
+		self.optimizations["gamma"][0] = np.mean(error*self.cached_data["x_norm"]) + self.regularizers["gamma"].map_data(self.vars["gamma"]) 
 
 		# Calculate the partial derivative of the error W.R.T the output from the previous layer.
-		dx_norm = error*self.gamma
-		var_inv = 1 / np.sqrt(self.cached_vars["var"]+self.epsilon)
-		x_mu = self.cached_vars["data"]-self.cached_vars["mu"]
+		dx_norm = error*self.vars["gamma"]
+		var_inv = 1 / np.sqrt(self.cached_data["var"]+self.vars["epsilon"])
+		x_mu = self.cached_data["data"]-self.cached_data["mu"]
 
-		dvar = np.sum(dx_norm*x_mu, axis=1, keepdims=True) * -0.5 * (self.cached_vars["var"]+self.epsilon)**(-3/2)
-		dmu = np.sum(dx_norm*-var_inv, axis=1, keepdims=True) + dvar*1 / self.cached_vars["data"].shape[1] * np.sum(-2*x_mu, axis=1, keepdims=True)
+		dvar = np.sum(dx_norm*x_mu, axis=1, keepdims=True) * -0.5 * (self.cached_data["var"]+self.vars["epsilon"])**(-3/2)
+		dmu = np.sum(dx_norm*-var_inv, axis=1, keepdims=True) + dvar*1 / self.cached_data["data"].shape[1] * np.sum(-2*x_mu, axis=1, keepdims=True)
 
 		# Return this layers error for the next layer to use.
-		return (dx_norm*var_inv) + (dmu/self.cached_vars["data"].shape[1]) + (dvar*2 / self.cached_vars["data"].shape[1]*x_mu)
+		return (dx_norm*var_inv) + (dmu/self.cached_data["data"].shape[1]) + (dvar*2 / self.cached_data["data"].shape[1]*x_mu)
 
  	# ------------------------------------------------------------------------------------------------------------------------
 	def update_vars(self, optimizer, epoch):
@@ -166,13 +150,14 @@ class Batchnorm(Base_layer):
 		Updates the trainable vars in the layer with the correct gradients.
 
 		Arguments:
-			optimizer - type = Instance of a class: A class that takes each layer's gradients and optimizes them to reach a local optima in the error faster.
-			epoch - type = Int: The current epoch that the layer is training on.
+			optimizer - instance of a class: A class that takes each layer's gradients and optimizes them to reach a local optima in the error faster.
+			epoch - int: The current epoch that the layer is training on.
 
 		"""
 
-		self.beta_optimizations = optimizer.optimize(self.beta_optimizations, epoch)
-		self.gamma_optimizations = optimizer.optimize(self.gamma_optimizations, epoch)
+		if self.trainable:
+			self.optimizations["gamma"] = optimizer.map_data(self.optimizations["gamma"], epoch)
+			self.optimizations["beta"] = optimizer.map_data(self.optimizations["beta"], epoch)
 
-		self.beta -= self.beta_optimizations[0]
-		self.gamma -= self.gamma_optimizations[0]
+			self.vars["gamma"] -= self.optimizations["gamma"][0]
+			self.vars["beta"] -= self.optimizations["beta"][0]
