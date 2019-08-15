14c9e05483b958d3f6b43955ce6a9e493d1218e2
Jonathan Andrews <48098383+Jonathan-Andrews@users.noreply.github.com>
2019-08-15 12:29:47 +0100

    Update base_layer.py

--- dlmb/layers/base_layer.py
+++ dlmb/layers/base_layer.py
@@ -2,7 +2,7 @@ from abc import ABCMeta, abstractmethod
 
 class Base_layer(metaclass=ABCMeta):
 	@abstractmethod
-	def __init__(self):
+	def __init__(self, *args, **kwargs):
 		"""
 		The Base_layer class is an abstract class and makes sure every layer uses the functions down below.
 
@@ -20,6 +20,35 @@ class Base_layer(metaclass=ABCMeta):
 
 		pass
 
+	# ------------------------------------------------------------------------------------------------------------------------
+	@abstractmethod
+	def get_summary(self, with_vars):
+		"""
+		get_summary() returns a summary of the layers features.
+
+		Arguments:
+			with_vars - bool: If True, get_summary() includes the layer's variables' values in the summary.
+
+		Returns:
+			summary - dict: A dictonary of the layers features.
+
+		"""
+
+		return summary
+
+	# ------------------------------------------------------------------------------------------------------------------------
+	@abstractmethod
+	def load(self, layer_data):
+		"""
+		Takes the layer_data from the model this layer belongs to, and sets all vars equal to each key in layer_data.
+
+		Arguments:
+			layer_data - dict: A dictonary of saved vars from when this layer was first built and then saved.
+
+		"""
+
+		pass
+
  	# ------------------------------------------------------------------------------------------------------------------------
 	@abstractmethod
 	def map_data(self, data):
@@ -27,10 +56,10 @@ class Base_layer(metaclass=ABCMeta):
 		Makes the layer map the incoming data to an output.
 
 		Arguments:
-			data - type = Numpy array: A matrix or vector (depending on batch size) containing real numbers passed from the previous layer.
+			data - Numpy array: A numpy array containing real numbers passed from the previous layer.
 
 		Returns:
-			output - type = Numpy array: A matrix or vector (depending on batch size) of outputs from this layer.
+			output - Numpy array: A numpy array of outputs from this layer.
 
 		"""
 
@@ -43,10 +72,10 @@ class Base_layer(metaclass=ABCMeta):
 		Makes the layer calculate the gradients of the error W.R.T each trainable var in the layer.
 
 		Arguments:
-			error - type = Numpy array: A matrix or vector (depending on batch size) containing the error for the previous layer.
+			error - Numpy array: A numpy array containing the error for the previous layer.
 
 		Returns:
-			output - type = Numpy array: A matrix or vector (depending on batch size) containing the error for this layer.
+			output - Numpy array: A numpy array containing the error for this layer.
 
 		"""
 
@@ -59,8 +88,8 @@ class Base_layer(metaclass=ABCMeta):
 		Makes the layer update all the trainable vars with the previously calculated gradients.
 
 		Arguments:
-			optimizer - type = Instance of a class: A class that takes each layer's gradients and optimizes them to reach a local optima in the error faster.
-			epoch - type = Int: The current epoch that the layer is training on.
+			optimizer - instance of a class: A class that takes each layer's gradients and optimizes them to reach a local optima in the error faster.
+			epoch - int: The current epoch that the layer is training on.
 
 		"""
 
