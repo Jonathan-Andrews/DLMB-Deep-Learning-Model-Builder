d4db29234a0df7e3d0636b38ab606bf099dbad89
Jonathan Andrews <48098383+Jonathan-Andrews@users.noreply.github.com>
2019-08-15 12:26:12 +0100

    Update optimizers.py

--- dlmb/optimizers.py
+++ dlmb/optimizers.py
@@ -1,7 +1,7 @@
 from abc import ABCMeta, abstractmethod
 import numpy as np
 
-# For all classes optimizations[0] is the calculated gradients and anything other than that is just optimizations from the optimizer class
+# For all classes optimizations[0] is the calculated gradients W.R.T the vars from the layer, and anything other than that is just optimizations from the optimizer class.
 
 class Base_optimizer(metaclass=ABCMeta):
 	@abstractmethod
@@ -15,9 +15,9 @@ class Base_optimizer(metaclass=ABCMeta):
 
 	# ------------------------------------------------------------------------------------------------------------------------
 	@abstractmethod
-	def optimize(self, optimizations, epoch):
+	def map_data(self, optimizations, epoch):
 		"""
-		This is where the math happens. The optimizer takes some data and applies a mathematical mapping to it.
+		This is where the math happens, the optimizer takes some data and applies a mathematical mapping to it.
 	
 		Arguments:
 			optimizations - type = Array: An array containing:
@@ -37,13 +37,13 @@ class Base_optimizer(metaclass=ABCMeta):
 class GD(Base_optimizer):
 	def __init__(self, lr=0.1, momentum=0.9):
 		"""
-		GD (Gradient Decent) is the simplest of all optimizers. 
-		Applies some momentum to the gradients of the vars that are being updated.
+		GD (Gradient Decent) is the simplest of all the optimizers. 
+		It applies some momentum to the gradients of the vars that are being updated.
 		It is reccomended to leave the arguments as they are.
 
 		Arguments:
-			lr - type = float: A float that takes a small fraction of the gradients. Controls how fast the model learns, however choosing a big lr isn't always good.
-			momentum - type = float: A float that controls the speed of convergence to a local minima, however choosing a big momentum isn't aslways good.
+			lr - float: A float that takes a small fraction of the gradients. Controls how fast the model learns, however choosing a big lr isn't always good.
+			momentum - float: A float that controls the speed of convergence to a local minima, however choosing a big momentum isn't aslways good.
 
 		"""
 
@@ -51,19 +51,19 @@ class GD(Base_optimizer):
 		self.momentum = momentum
 
 	# ------------------------------------------------------------------------------------------------------------------------
-	def optimize(self, optimizations, epoch):
+	def map_data(self, optimizations, epoch):
 		"""
 		Applies some momentum to the gradients so convergence to the local minima of the loss function will be quicker.
 
 		Arguments:
-			optimizations - type = Array: An array containing:
-				gradients - type = Numpy array: A numpy array of gradients for the vars that are being updated.
-				optimizations - type = Numpy array: A numpy array of optimizations from the previous time step.
+			optimizations - list: A list containing:
+				gradients - Numpy array: A numpy array of gradients for the vars that are being updated.
+				optimizations - Numpy array: A numpy array of optimizations from the previous time step.
 
 		Returns:
-			output - type = Array:  An array containing:
-				updates - type = Numpy array: A numpy of the optimized gradients that will be used to update the vars.
-				optimizations - type = Numpy array: A numpy array of optimizations from this time step.
+			output - list: A list containing:
+				updates - Numpy array: A numpy of the optimized gradients that will be used to update the vars.
+				optimizations - Numpy array: A numpy array of optimizations from this time step.
 
 		"""
 
@@ -78,9 +78,9 @@ class RMSprop(Base_optimizer):
 		It is reccomended to leave the arguments as they are.
 
 		Arguments:
-			lr: A learning rate that takes a small fraction of the gradients. Controls how fast the model learns, however choosing a big lr isn't always good.
-			beta: Helps the gradients find the optimal direction to head in.
-			epsilon: A very small number that helps to stop division by zero.
+			lr - float: A learning rate that takes a small fraction of the gradients. Controls how fast the model learns, however choosing a big lr isn't always good.
+			beta - float: Helps the gradients find the optimal direction to head in.
+			epsilon - float: A very small number that helps to stop division by zero.
 
 		"""
 
@@ -89,19 +89,19 @@ class RMSprop(Base_optimizer):
 		self.epsilon = epsilon
 
 	# ------------------------------------------------------------------------------------------------------------------------
-	def optimize(self, optimizations, epoch):
+	def map_data(self, optimizations, epoch):
 		"""
 		Applies a momentum like operation to the gradients so the gradients can find the optimal direction to head in.
 
 		Arguments:
-			optimizations - type = Array: An array containing:
-				gradients - type = Numpy array: A numpy array of gradients for the vars that are being updated.
-				optimizations - type = Numpy array: A numpy array of optimizations from the previous time step.
+			optimizations - list: A list containing:
+				gradients - Numpy array: A numpy array of gradients for the vars that are being updated.
+				optimizations - Numpy array: A numpy array of optimizations from the previous time step.
 
 		Returns:
-			output - type = Array:  An array containing:
-				updates - type = Numpy array: A numpy of the optimized gradients that will be used to update the vars.
-				optimizations - type = Numpy array: A numpy array of optimizations from this time step.
+			output - list: A list containing:
+				updates - Numpy array: A numpy of the optimized gradients that will be used to update the vars.
+				optimizations - Numpy array: A numpy array of optimizations from this time step.
 
 		"""
 
@@ -116,10 +116,10 @@ class Adam(Base_optimizer):
 		It is reccomended to leave the arguments as they are.
 
 		Arguments:
-			lr: A learning rate that takes a small fraction of the gradients. Controls how fast the model learns, however choosing a big lr isn't always good.
-			momentum: How much momentum to add to the gradients.
-			beta: Helps the gradients find the optimal direction to head in.
-			epsilon: A very small number that helps to stop division by zero. 
+			lr - float: A learning rate that takes a small fraction of the gradients. Controls how fast the model learns, however choosing a big lr isn't always good.
+			momentum - float: How much momentum to add to the gradients.
+			beta - float: Helps the gradients find the optimal direction to head in.
+			epsilon - float - float: A very small number that helps to stop division by zero. 
 
 		"""
 
@@ -129,25 +129,25 @@ class Adam(Base_optimizer):
 		self.epsilon = epsilon
 
 	# ------------------------------------------------------------------------------------------------------------------------
-	def optimize(self, optimizations, epoch):
+	def map_data(self, optimizations, epoch):
 		"""
 		Applies momentum and a momentum like operation to the gradients so the gradients can find the optimal direction to head in.
 
 		Arguments:
-			optimizations - type = Array: An array containing:
-				gradients - type = Numpy array: A numpy array of gradients for the vars that are being updated.
-				beta - type = Numpy array: A numpy array of beta optimizations from the previous time step.
-				momentum - type = Numpy array: A numpy array of momentum optimizations from the previous time step.
+			optimizations - list: A lsit containing:
+				gradients - Numpy array: A numpy array of gradients for the vars that are being updated.
+				beta - Numpy array: A numpy array of beta optimizations from the previous time step.
+				momentum - Numpy array: A numpy array of momentum optimizations from the previous time step.
 
 		Returns:
-			output - type = Array:  An array containing:
-				updates - type = Numpy array: A numpy of the optimized gradients that will be used to update the vars.
-				beta - type = Numpy array: A numpy array of beta optimizations from this time step.
-				momentum - type = Numpy array: A numpy array of momentum optimizations from this time step.
+			output - list: A list containing:
+				updates - Numpy array: A numpy of the optimized gradients that will be used to update the vars.
+				beta - Numpy array: A numpy array of beta optimizations from this time step.
+				momentum - Numpy array: A numpy array of momentum optimizations from this time step.
 
 		"""
 
-		# Checks if the momentum optimizations aren't there, meaning this is the first time the optimizer is getting used within this train loop
+		# Checks if the momentum optimizations aren't there, meaning this is the first time the optimizer is getting used within this train loop.
 		if len(optimizations) < 3:
 			optimizations.append(0)
 
@@ -158,3 +158,29 @@ class Adam(Base_optimizer):
 		corrected_momentum = optimizations[2] / (1-self.momentum**epoch)+self.epsilon
 
 		return [self.lr * (corrected_momentum / (np.sqrt(corrected_RMSprop)+self.epsilon)), optimizations[1], optimizations[2]]
+
+# ------------------------------------------------------------------------------------------------------------------------
+def get(optimizer):
+	"""
+	Finds and returns the correct optimizer class.
+
+	Arguments:
+		optimizer - str/instance of a class: The optimization class.
+
+	Returns:
+		optimizer - instance of a class: The correct optimization class.
+		
+	"""
+
+	if type(optimizer) == str:
+		if optimizer.lower() in ("gd"):
+			return GD()
+		elif optimizer.lower() in ("rmsprop"):
+			return RMSprop()
+		elif optimizer.lower() in ("adam"):
+			return Adam()
+		else:
+			print("'%s' is not currently an available optimizer. Has been set to 'GD' by default" % optimizer)
+			return GD()
+	else:
+		return optimizer
