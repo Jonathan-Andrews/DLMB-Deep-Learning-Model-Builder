7bcd9636045d218149a35946d7a4aa615654ca04
Jonathan Andrews <48098383+Jonathan-Andrews@users.noreply.github.com>
2019-08-15 12:30:37 +0100

    Update dense.py

--- dlmb/layers/dense.py
+++ dlmb/layers/dense.py
@@ -4,188 +4,177 @@ from layers.base_layer import Base_layer
 import activations as A
 import initializers as I
 import regularizers as R 
-# Always breath AIR. breathing AIR is good.
 
 class Dense(Base_layer):
-	def __init__(self, layer_shape, activation=A.Linear(), bias_type="per_node",
+	def __init__(self, output_size, input_shape=1, bias_type="per_node", trainable=True, activation="Linear",
 				 weight_initializer="uniform", bias_initializer="uniform",
-				 weight_regularizer="L0", bias_regularizer="L0"):
+	             weight_regularizer="L0", bias_regularizer="L0", *args, **kwargs):
 
 		"""
-		The dense class is one of the most simple neural-net layers. 
-		It takes in an input and returns an output with the form of g(X*W + B).
+		The Dense layer is one of the most simplest layers. 
 
 		Arguments:
-			layer_shape - type = tuple: A tuple of the layer shape, (E.X (6, 2), 6 is the number of nodes in the layer and 2 is the number of incoming inputs).
-			activation - type = Instance of a class: An activation function that generally applies a non-linear function to the layer's output.
-			bias_type - type = Str: Has three settings, per_node (a normal bias type), single (one bias weight for all nodes in this layer), none (no bias is used).
-			weight_initializer - type = Str: An initializer function to set the values for the weights.
-			bias_initializer - type = Str: An initializer function to set the values for the bias'.
-			weight_regularizer - type = Str: A regularizer function that regularizes the weights.
-			bias_regularizer - type = Str: A regularizer function that regularizes the bias'.
+			output_size - int: The number of outputs for this layer.
+			input_shape - int/tuple: The shape (excluding batch size) of the input data this layer will receive.
+			bias_type - string: Has three settings, per_node (a bias with weights for every output), single (one bias weight for all outputs), none (no bias is used).
+			trainable - bool: If set to True, the vars in this layer will update based on the calculated loss of this layer W.R.T the vars.
+			activation - instance of an activation class: A mathematical function that generally applies a non-linear mapping to the functions input.
+			weight_initializer - str/instance of a function: An initializer function to set the values for the weights.
+			bias_initializer - str/instance of a function: An initializer function to set the values for the bias'.
+			weight_regularizer - str/instance of a regularizer class: A regularizer function that regularizes the weights.
+			bias_regularizer - str/instance of a regularizer class: A regularizer function that regularizes the bias'.
 
 		"""
 
-		self.layer_type = "Dense"
+		# Flattens the input_shape if data type is tuple.
+		if type(input_shape) == tuple:
+			shape = 1
+			for i in range(len(input_shape)):
+				shape *= input_shape[i]
+			input_shape = shape
+
+		self.name = "Dense"
 		self.built = False
-		self.layer_shape = layer_shape
-		self.activation = activation
+		self.layer_shape = (output_size, input_shape)
 		self.bias_type = bias_type
-		self.weight_initializer = weight_initializer
-		self.bias_initializer = bias_initializer
-		self.weight_regularizer = weight_regularizer
-		self.bias_regularizer = bias_regularizer
+		self.trainable = trainable
 
-		# Stores the optimizations for the trainable vars.
-		self.weight_optimizations = [0,0]
-		self.bias_optimizations = [0,0]
+		self.activation = A.get(activation)
+		self.initializers = {"weight":weight_initializer, "bias":bias_initializer}
+		self.regularizers = {"weight":R.get(weight_regularizer), "bias":R.get(bias_regularizer)}
 
-		# Stores the weights and bias
-		self.weights = []
-		self.bias = []
-
-		# Stores the mapped data in a dict.
-		self.cached_vars = {}
+		self.vars = {"weight":0, "bias":0}
+		self.optimizations = {"weight":[0,0], "bias":[0,0]}
+		self.cached_data = {}
 
  	# ------------------------------------------------------------------------------------------------------------------------
-	def set_vars(self):
+	def set_vars(self, *args, **kwargs):
 		"""
 		Sets the values for all of the vars that will be used.
 		Also sets the layers activation function if the user sent through a string as the argument.
 
 		"""
 
-		if self.built != True:
-			# Sets the weights based on self.weight_initializer.
-			if self.weight_initializer.lower() in ("zeros", "zero"):
-				self.weights = I.zeros(self.layer_shape)
-			elif self.weight_initializer.lower() in ("ones", "one"):
-				self.weights = I.ones(self.layer_shape)
-			elif self.weight_initializer.lower() in ("random"):
-				self.weights = I.random(self.layer_shape)
-			elif self.weight_initializer.lower() in ("uniform"):
-				self.weights = I.uniform(self.layer_shape)
-			elif self.weight_initializer.lower() in ("xavier"):
-				self.weights = I.xavier(self.layer_shape)
-			elif self.weight_initializer.lower() in ("xavier_uniform"):
-				self.weights = I.xavier_uniform(self.layer_shape)
-			elif self.weight_initializer.lower() in ("sigmoid_uniform"):
-				self.weights = I.sigmoid_uniform(self.layer_shape)
-			elif self.weight_initializer.lower() in ("relu"):
-				self.weights = I.relu(self.layer_shape)
-			elif self.weight_initializer.lower() in ("relu_uniform"):
-				self.weights = I.relu_uniform(self.layer_shape)
+		if not self.built:
+			# Set self.vars["weight"] based on self.initializers["weight"].
+			self.vars["weight"] = I.get(self.initializers["weight"])(self.layer_shape)
+
+			# Set self.vars["bias"] based on self.initializers["bias"].
+			self.vars["bias"] = I.get(self.initializers["bias"])((self.layer_shape[0], 1))
+
+			# Set self.vars["bias"] based on self.bias_type.
+			if self.bias_type == "per_node":
+				self.vars["bias"] = self.vars["bias"]
+			elif self.bias_type == "single":
+				self.vars["bias"] = self.vars["bias"][0]
+			elif self.bias_type == "none":
+				self.vars["bias"] == np.array([[0]])
 			else:
-				raise ValueError(f"{self.weight_initializer} is not currently an available weight initializer.")
-
-			# Sets the bias' based on self.bias_initializer.
-			if self.bias_initializer.lower() in ("zeros", "zero"):
-				self.bias = I.zeros((self.layer_shape[0],1))
-			elif self.bias_initializer.lower() in ("ones", "one"):
-				self.bias = I.ones((self.layer_shape[0],1))
-			elif self.bias_initializer.lower() in ("random"):
-				self.bias = I.random((self.layer_shape[0],1))
-			elif self.bias_initializer.lower() in ("uniform"):
-				self.bias = I.uniform((self.layer_shape[0],1))
-			elif self.bias_initializer.lower() in ("xavier"):
-				self.bias = I.xavier((self.layer_shape[0],1))
-			elif self.bias_initializer.lower() in ("xavier_uniform"):
-				self.bias = I.xavier_uniform((self.layer_shape[0],1))
-			elif self.bias_initializer.lower() in ("sigmoid_uniform"):
-				self.bias = I.sigmoid_uniform((self.layer_shape[0],1))
-			elif self.bias_initializer.lower() in ("relu"):
-				self.bias = I.relu((self.layer_shape[0],1))
-			elif self.bias_initializer.lower() in ("relu_uniform"):
-				self.bias = I.relu_uniform((self.layer_shape[0],1))
-			else:
-				raise ValueError(f"{self.bias_initializer} is not currently an available bias initializer.")
-
-		# Sets the layers activation function if the user sent through a string as the argument.
-		if type(self.activation) == str:
-			if self.activation.lower() in ("linear"):
-				self.activation = A.Linear()
-			elif self.activation.lower() in ("softmax"):
-				self.activation = A.Softmax()
-			elif self.activation.lower() in ("sigmoid"):
-				self.activation = A.Sigmoid()
-			elif self.activation.lower() in ("tanh"):
-				self.activation = A.Tanh()
-			elif self.activation.lower() in ("relu"):
-				self.activation = A.ReLU()
-			elif self.activation.lower() in ("leaky_relu"):
-				self.activation = A.Leaky_ReLU()
-			elif self.activation.lower() in ("elu"):
-				self.activation = A.ELU()
-			else:
-				raise ValueError(f"{self.activation} is not currently an available activation function.")
-
-		# Sets the regularizers if the user sent through a string as the argument.
-		if type(self.weight_regularizer) == str:
-			if self.weight_regularizer.lower() in ("l0"):
-				self.weight_regularizer = R.L0()
-			elif self.weight_regularizer.lower() in ("l1"):
-				self.weight_regularizer = R.L1()
-			elif self.weight_regularizer.lower() in ("l2"):
-				self.weight_regularizer = R.L2()
-
-		if type(self.bias_regularizer) == str:
-			if self.bias_regularizer.lower() in ("l0"):
-				self.bias_regularizer = R.L0()
-			elif self.bias_regularizer.lower() in ("l1"):
-				self.bias_regularizer = R.L1()
-			elif self.bias_regularizer.lower() in ("l2"):
-				self.bias_regularizer = R.L2()
+				print("At Dense.set_vars(): '%s' is not a valid bias_type. Has been set to 'per_node' by default" % self.bias_type)
+		else:
+			for key in self.vars:
+				self.vars[key] = np.asarray(self.vars[key])
 
 		self.built = True
 
- 	# ------------------------------------------------------------------------------------------------------------------------
-	def map_data(self, data):
+	# ------------------------------------------------------------------------------------------------------------------------
+	def get_summary(self, with_vars, *args, **kwargs):
 		"""
-		Maps the data to an output in the form of g(X*W + B) where g(x) is self.activation.
+		get_summary() returns a summary of the layers features.
 
 		Arguments:
-			data - type = Numpy array: A matrix or vector (depending on batch size) containing real numbers passed from the previous layer.
+			with_vars - type = Bool: If True, get_summary() includes the layer's variables' values in the summary.
 
 		Returns:
-			output - type = Numpy array: A matrix or vector (depending on batch size) of outputs from this layer.
+			summary - type = Dict: A dictonary of the layers features.
+
+		"""
+
+		summary = {
+					"name":self.name,
+					"built":self.built,
+					"layer_shape":self.layer_shape,
+					"bias_type":self.bias_type,
+					"trainable":self.trainable,
+					"activation":self.activation.name,
+					"initializers":self.initializers,
+					"regularizers":{key:self.regularizers[key].name for key in self.regularizers},
+					"num_trainable_vars":(self.vars["weight"].shape[0]*self.vars["weight"].shape[1])+self.vars["bias"].shape[0]
+				  }
+
+		if with_vars:
+			summary["vars"] = {key:np.asarray(self.vars[key]).tolist() for key in self.vars}
+			summary["optimizations"] = {key:np.asarray(self.optimizations[key]).tolist() for key in self.optimizations}
+
+		return summary
+
+	# ------------------------------------------------------------------------------------------------------------------------
+	def load(self, layer_data, *args, **kwargs):
+		"""
+		Takes the layer_data from the model this layer belongs to, and sets all vars equal to each key in layer_data.
+
+		Arguments:
+			layer_data - dict: A dictonary of saved vars from when this layer was first built and then saved.
+
+		"""
+
+		self.name = layer_data["name"]
+		self.built = layer_data["built"]
+		self.layer_shape = layer_data["layer_shape"]
+		self.bias_type = layer_data["bias_type"]
+		self.trainable = layer_data["trainable"]
+		self.activation = A.get(layer_data["activation"])
+		self.initializers = layer_data["initializers"]
+		self.regularizers = {key:R.get(layer_data["regularizers"][key]) for key in layer_data["regularizers"]}
+		self.vars = {key:np.asarray(layer_data["vars"][key]) for key in layer_data["vars"]}
+		self.optimizations = {key:np.asarray(layer_data["optimizations"][key]) for key in layer_data["optimizations"]}
+
+ 	# ------------------------------------------------------------------------------------------------------------------------
+	def map_data(self, data, *args, **kwargs):
+		"""
+		Makes the layer map the incoming data to an output.
+
+		Arguments:
+			data - Numpy array: A numpy array containing real numbers passed from the previous layer.
+
+		Returns:
+			output - Numpy array: A numpy array of outputs from this layer.
 
 		"""
 
 		# Makes sure that the data is the right shape.
 		if len(data.shape) == 1:
 			data = data.reshape((1, data.shape[0]))
-		elif len(data.shape) == 2:
-			data = data
-		elif len(data.shape) == 3:
-			data = data.reshape((data.shape[0], data.shape[1]*data.shape[2]))
-		else:
-			raise ValueError(f"data with size {data.shape} can not be used. Please reshape the data to a 1dim, 2dim or 3dim sized numpy array.")
+		elif len(data.shape) > 1:
+			length = 1
+			for i in range(len(data.shape)-1):
+				length *= data.shape[i+1]
+			data = data.reshape((data.shape[0], length))
 
-		# Makes sure the size of the columns of the data is the same as the number of incoming inputs specified in self.layer_shape.
-		if data.shape[1] == self.layer_shape[1]:
-			self.cached_vars["data"] = data
-			self.cached_vars["mapped_data"] = np.dot(data, self.weights.T) + self.bias.T
-			return self.activation.function(self.cached_vars["mapped_data"])
+		if self.layer_shape[1] == data.shape[1]:
+			self.cached_data["data"] = data
+			self.cached_data["mapped_data"] = np.dot(data, self.vars["weight"].T) + self.vars["bias"].T
+			return self.activation.map_data(self.cached_data["mapped_data"])
 		else:
-			raise ValueError(f"The data with size {data.shape} can not be used. The columns must match the incoming inputs for the layer specified in self.layer_shape.")
+			raise ValueError("At Dense.map_data(): Data with shape %s does not match this layers specified input_shape of %s" % (data.shape, self.layer_shape[1]))
 
  	# ------------------------------------------------------------------------------------------------------------------------
-	def calculate_gradients(self, error):
+	def calculate_gradients(self, error, *args, **kwargs):
 		"""
-		Calculates the partial derivative of the error W.R.T each trainable var in the layer. 
+		Calculates the gradients of the error W.R.T each trainable var in the layer.
 		In the dense layer the partial derivative of the error W.R.T each trainable var is dE/dO * dO/dZ * dZ/dW
 
 		Arguments:
-			error - type = Numpy array: A matrix or vector (depending on batch size) containing the error for the previous layer.
+			error - Numpy array: A numpy array containing the error for the previous layer.
 
 		Returns:
-			output - type = Numpy array: A matrix or vector (depending on batch size) containing the error for this layer.
+			output - Numpy array: A numpy array containing the error for this layer.
 
 		"""
 
-		dO_dZ = self.activation.derivative(self.cached_vars["mapped_data"])
+		dO_dZ = self.activation.calculate_gradients(self.cached_data["mapped_data"])
 
-		# Checks if the activation function was softmax, becuase the derivative of softmax returns a 3dim matrix.
+		# Checks if the activation function was softmax, becuase the derivative of softmax returns a 3dim numpy array.
 		if len(dO_dZ.shape) == 3:
 			error = error.reshape((error.shape[0], error.shape[1], 1))
 			node_errors = np.sum(dO_dZ*error, axis=1)
@@ -193,31 +182,33 @@ class Dense(Base_layer):
 			node_errors = dO_dZ*error
 
 		# Calculate the weight gradients.
-		self.weight_optimizations[0] = np.dot(node_errors.T, self.cached_vars["data"])/node_errors.shape[0] + self.weight_regularizer.regularize(self.weights)
+		self.optimizations["weight"][0] = np.dot(node_errors.T, self.cached_data["data"])/node_errors.shape[0] + self.regularizers["weight"].map_data(self.vars["weight"])
 
 		# Calculate the bias gradients based on self.bias_type.
 		if self.bias_type == "per_node":
-			self.bias_optimizations[0] = np.mean(node_errors.T, axis=1, keepdims=True) + self.bias_regularizer.regularize(self.bias)
+			self.optimizations["bias"][0] = np.mean(node_errors.T, axis=1, keepdims=True) + self.regularizers["bias"].map_data(self.vars["bias"])
 		elif self.bias_type == "single":
-			self.bias_optimizations[0] = np.mean(node_errors) + self.bias_regularizer.regularize(self.bias)
-		elif self.bias_type == "none":
-			self.bias_optimizations[0] = 0
+			self.optimizations["bias"][0] = np.mean(np.sum(node_errors, axis=1)) + self.regularizers["bias"].map_data(self.vars["bias"])
+		else:
+			self.optimizations["bias"][0] = np.array([[0.0]])
 
 		# Return this layer's error for the next layer to use.
-		return np.dot(node_errors, self.weights)
+		return np.dot(node_errors, self.vars["weight"])
 
  	# ------------------------------------------------------------------------------------------------------------------------
-	def update_vars(self, optimizer, epoch):
+	def update_vars(self, optimizer, epoch, *args, **kwargs):
 		"""
 		Updates the trainable vars in the layer with the correct gradients.
 
 		Arguments:
-			optimizer - type = Instance of a class: A class that takes each layer's gradients and optimizes them to reach a local optima in the error faster.
-			epoch - type = Int: The current epoch that the layer is training on.
+			optimizer - instance of a class: A class that takes each layer's gradients and optimizes them to reach a local optima in the error faster.
+			epoch - int: The current epoch that the layer is training on.
 
 		"""
-		self.weight_optimizations = optimizer.optimize(self.weight_optimizations, epoch)
-		self.bias_optimizations = optimizer.optimize(self.bias_optimizations, epoch)
 
-		self.weights = self.weights - self.weight_optimizations[0]
-		self.bias = self.bias - self.bias_optimizations[0]
+		if self.trainable:
+			self.optimizations["weight"] = optimizer.map_data(self.optimizations["weight"], epoch)
+			self.optimizations["bias"] = optimizer.map_data(self.optimizations["bias"], epoch)
+
+			self.vars["weight"] -= self.optimizations["weight"][0]
+			self.vars["bias"] -= self.optimizations["bias"][0]
