14a5aeda0ad90ef4d33b87cd3affa87b3ab88a4d
Jonathan Andrews <324MrL324@gmail.com>
2020-01-03 19:05:05 +0000

    Update initializers.py

--- dlmb/initializers.py
+++ dlmb/initializers.py
@@ -1,153 +1,163 @@
 import numpy as np
+from utils.helpers import *
 
-def zeros(matrix_shape):
+@accepts(shape=tuple)
+def zeros(shape) -> np.ndarray:
 	"""
-	Creates a numpy array of shape matrix_shape filled with values of 0.
+		Creates a numpy array of shape shape filled with values of 0.
 
-	Arguments:
-		matrix_shape - tuple: A tuple with 2 numbers, specifying size of the numpy array.
+		Arguments:
+			shape : tuple : A tuple with 2 numbers, specifying size of the numpy array.
 
-	Returns:
-		output - Numpy array: A numpy array filled with values of 0.
+		Returns:
+			output : np.ndarray : A numpy array filled with values of 0.
 		
 	"""
 
-	return np.zeros(matrix_shape)
+	return np.zeros(shape)
 
-# ------------------------------------------------------------------------------------------------------------------------
-def ones(matrix_shape):
+
+@accepts(shape=tuple)
+def ones(shape) -> np.ndarray:
 	"""
-	Creates a numpy array of shape matrix_shape filled with values of 1.
+		Creates a numpy array of shape shape filled with values of 1.
 
-	Arguments:
-		matrix_shape - tuple: A tuple with 2 numbers, specifying size of the numpy array.
+		Arguments:
+			shape : tuple : A tuple with 2 numbers, specifying size of the numpy array.
 
-	Returns:
-		output - Numpy array: A numpy array filled with values of 1.
+		Returns:
+			output : Numpy array : A numpy array filled with values of 1.
 		
 	"""
 
-	return np.ones(matrix_shape)
+	return np.ones(shape)
 
-# ------------------------------------------------------------------------------------------------------------------------
-def random(matrix_shape):
+
+@accepts(shape=tuple)
+def random(shape) -> np.ndarray:
 	"""
-	Creates a numpy array of shape matrix_shape filled with random values between 0 and 1.
+		Creates a numpy array of shape shape filled with random values between 0 and 1.
 
-	Arguments:
-		matrix_shape - tuple: A tuple with 2 numbers, specifying size of the numpy array.
+		Arguments:
+			shape : tuple : A tuple with 2 numbers, specifying size of the numpy array.
 
-	Returns:
-		output - Numpy array: A numpy array filled with random values between 0 and 1.
+		Returns:
+			output : np.ndarray : A numpy array filled with random values between 0 and 1.
 		
 	"""
 
-	return np.random.random(matrix_shape)
+	return np.random.random(shape)
 
-# ------------------------------------------------------------------------------------------------------------------------
-def uniform(matrix_shape):
+
+@accepts(shape=tuple)
+def uniform(shape) -> np.ndarray:
 	"""
-	Creates a uniform numpy array with boundaries of [-1/sqrt(n), 1/sqrt(n)].
+		Creates a uniform numpy array with boundaries of [-1/sqrt(n), 1/sqrt(n)].
 
-	Arguments:
-		matrix_shape - tuple: A tuple with 2 numbers, specifying size of the numpy array.
+		Arguments:
+			shape : tuple : A tuple with 2 numbers, specifying size of the numpy array.
 
-	Returns:
-		output - Numpy array: A uniform numpy array.
+		Returns:
+			output : np.ndarray : A uniform numpy array.
 		
 	"""
 
-	boundaries = 1/np.sqrt(matrix_shape[1])
-	return np.random.uniform(-boundaries, boundaries, matrix_shape)
+	boundaries = 1/np.sqrt(shape[1])
+	return np.random.uniform(-boundaries, boundaries, shape)
 
-# ------------------------------------------------------------------------------------------------------------------------
-def xavier(matrix_shape):
+
+@accepts(shape=tuple)
+def xavier(shape) -> np.ndarray:
 	"""
-	Creates a gaussian distribution numpy array with a mean of 0 and variance of sqrt(2/(n+m)).
+		Creates a gaussian distribution numpy array with a mean of 0 and variance of sqrt(2/(n+m)).
 
-	Arguments:
-		matrix_shape - tuple: A tuple with 2 numbers, specifying size of the numpy array.
+		Arguments:
+			shape : tuple : A tuple with 2 numbers, specifying size of the numpy array.
 
-	Returns:
-		output - Numpy array: A gaussian distribution numpy array.
+		Returns:
+			output : np.ndarray : A uniform numpy array.
 		
 	"""
 
-	return np.random.normal(0, np.sqrt(2/(matrix_shape[0]+matrix_shape[1])), matrix_shape)
+	return np.random.normal(0, np.sqrt(2/(shape[0]+shape[1])), shape)
 
-# ------------------------------------------------------------------------------------------------------------------------
-def xavier_uniform(matrix_shape):
+
+@accepts(shape=tuple)
+def xavier_uniform(shape) -> np.ndarray:
 	"""
-	Creates a uniform numpy array based of the xavier initializer with boundaries of [-sqrt(6)/sqrt(n+m), sqrt(6)/sqrt(n+m)].
+		Creates a uniform numpy array based of the xavier initializer with boundaries of [-sqrt(6)/sqrt(n+m), sqrt(6)/sqrt(n+m)].
 
-	Arguments:
-		matrix_shape - tuple: A tuple with 2 numbers, specifying size of the numpy array.
+		Arguments:
+			shape : tuple : A tuple with 2 numbers, specifying size of the numpy array.
 
-	Returns:
-		output - Numpy array: A uniform numpy array.
+		Returns:
+			output : np.ndarray : A uniform numpy array.
 		
 	"""
 
-	boundaries = np.sqrt(6)/np.sqrt(matrix_shape[0]+matrix_shape[1])
-	return np.random.uniform(-boundaries, boundaries, matrix_shape)
+	boundaries = np.sqrt(6)/np.sqrt(shape[0]+shape[1])
+	return np.random.uniform(-boundaries, boundaries, shape)
 
-# ------------------------------------------------------------------------------------------------------------------------
-def sigmoid_uniform(matrix_shape):
+
+@accepts(shape=tuple)
+def sigmoid_uniform(shape) -> np.ndarray:
 	"""
-	Creates a uniform numpy array based of the xavier_uniform initializer with boundaries of [-(4*sqrt(6))/sqrt(n+m), 4*sqrt(6)/sqrt(n+m)].
+		Creates a uniform numpy array based of the xavier_uniform initializer with boundaries of [-(4*sqrt(6))/sqrt(n+m), 4*sqrt(6)/sqrt(n+m)].
 
-	Arguments:
-		matrix_shape - tuple: A tuple with 2 numbers, specifying size of the numpy array.
+		Arguments:
+			shape : tuple : A tuple with 2 numbers, specifying size of the numpy array.
 
-	Returns:
-		output - Numpy array: A uniform numpy array.
+		Returns:
+			output : np.ndarray : A uniform numpy array.
 		
 	"""
 
-	boundaries = 4*np.sqrt(6)/np.sqrt(matrix_shape[0]+matrix_shape[1])
-	return np.random.uniform(-boundaries, boundaries, matrix_shape)
+	boundaries = 4*np.sqrt(6)/np.sqrt(shape[0]+shape[1])
+	return np.random.uniform(-boundaries, boundaries, shape)
 
-# ------------------------------------------------------------------------------------------------------------------------
-def relu(matrix_shape):
+
+@accepts(shape=tuple)
+def relu(shape) -> np.ndarray:
 	"""
-	Creates a gaussian distribution numpy array with a mean of 0 and variance of sqrt(2/m).
+		Creates a gaussian distribution numpy array with a mean of 0 and variance of sqrt(2/m).
 
-	Arguments:
-		matrix_shape - tuple: A tuple with 2 numbers, specifying size of the numpy array.
+		Arguments:
+			shape : tuple : A tuple with 2 numbers, specifying size of the numpy array.
 
-	Returns:
-		output - Numpy array: A gaussian distribution numpy array.
+		Returns:
+			output : np.ndarray : A uniform numpy array.
 		
 	"""
 
-	return np.random.normal(0, np.sqrt(2/matrix_shape[0]), matrix_shape)
+	return np.random.normal(0, np.sqrt(2/shape[1]), shape)
 
-# ------------------------------------------------------------------------------------------------------------------------
-def relu_uniform(matrix_shape):
+
+@accepts(shape=tuple)
+def relu_uniform(shape):
 	"""
-	Creates a uniform numpy array based of the relu initializer with boundaries of [-sqrt(6/m), sqrt(6/m)].
+		Creates a uniform numpy array based of the relu initializer with boundaries of [-sqrt(6/m), sqrt(6/m)].
+		
+		Arguments:
+			shape : tuple : A tuple with 2 numbers, specifying size of the numpy array.
 
-	Arguments:
-		matrix_shape - tuple: A tuple with 2 numbers, specifying size of the numpy array.
-
-	Returns:
-		output - Numpy array: A uniform numpy array.
+		Returns:
+			output : np.ndarray : A uniform numpy array.
 		
 	"""
 
-	boundaries = np.sqrt(6/matrix_shape[0])
-	return np.random.uniform(-boundaries, boundaries, matrix_shape)
+	boundaries = np.sqrt(6/shape[1])
+	return np.random.uniform(-boundaries, boundaries, shape)
 
-# ------------------------------------------------------------------------------------------------------------------------
+
 def get(initializer):
 	"""
-	Finds and returns the correct initializer function.
+		Finds and returns the correct initializer function.
 
-	Arguments:
-		initializer - str/instance of a function: The initializer function.
+		Arguments:
+			initializer : str/callable : The initializer function.
 
-	Returns:
-		initializer - instance of a function: The correct initializer function.
+		Returns:
+			initializer : callable : The correct initializer function.
 		
 	"""
 
@@ -160,9 +170,9 @@ def get(initializer):
 			return random
 		elif initializer.lower() in ("uniform"):
 			return uniform
-		elif initializer.lower() in ("xavier"):
+		elif initializer.lower() in ("xavier", "glorot"):
 			return xavier
-		elif initializer.lower() in ("xavier_uniform"):
+		elif initializer.lower() in ("xavier_uniform", "glorot_uniform"):
 			return xavier_uniform
 		elif initializer.lower() in ("sigmoid_uniform"):
 			return sigmoid_uniform
@@ -171,7 +181,7 @@ def get(initializer):
 		elif initializer.lower() in ("relu_uniform"):
 			return relu_uniform
 		else:
-			print("'%s' is not currently an available initializer function. Has been set to 'random' by default" % initializer)
-			return random
+			print("'%s' is not currently an available initializer function. Has been set to 'uniform' by default" % initializer)
+			return uniform
 	else:
 		return initializer
